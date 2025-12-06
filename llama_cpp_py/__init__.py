8b8e0a61a10a855a839cb425108acbc2b6c6b3ab
sergey21000 <67040429+sergey21000@users.noreply.github.com>
2025-12-06 19:32:55 +0300

    fix

--- llama_cpp_py/__init__.py
+++ llama_cpp_py/__init__.py
@@ -3,4 +3,5 @@ from llama_cpp_py.server.sync import LlamaSyncServer
 from llama_cpp_py.server.async_ import LlamaAsyncServer
 from llama_cpp_py.client.async_ import LlamaAsyncClient
 
+
 __all__ = ['LlamaSyncServer', 'LlamaAsyncServer', 'LlamaReleaseManager']
--- llama_cpp_py/client/base.py
+++ llama_cpp_py/client/base.py
@@ -1,5 +1,5 @@
 import re
-
+from pathlib import Path
 
 
 class LlamaClientBase:
