cbd604dcc41122d529df42190a63d69d5d16fbbd
sergey21000 <67040429+sergey21000@users.noreply.github.com>
2025-12-06 19:30:00 +0300

    Update __init__.py

--- llama_cpp_py/__init__.py
+++ llama_cpp_py/__init__.py
@@ -1,5 +1,6 @@
+from llama_cpp_py.release_manager.manager import LlamaReleaseManager
 from llama_cpp_py.server.sync import LlamaSyncServer
 from llama_cpp_py.server.async_ import LlamaAsyncServer
-from llama_cpp_py.release_manager.manager import LlamaReleaseManager
+from llama_cpp_py.client.async_ import LlamaAsyncClient
 
 __all__ = ['LlamaSyncServer', 'LlamaAsyncServer', 'LlamaReleaseManager']
