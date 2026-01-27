8834aa97ae8f958cf847457cf9fe5a01dd624607
sergey21000 <67040429+sergey21000@users.noreply.github.com>
2026-01-27 15:57:38 +0300

    refactor

--- llama_cpp_py/__init__.py
+++ llama_cpp_py/__init__.py
@@ -2,6 +2,7 @@ from llama_cpp_py.release_manager.manager import LlamaReleaseManager
 from llama_cpp_py.server.sync import LlamaSyncServer
 from llama_cpp_py.server.async_ import LlamaAsyncServer
 from llama_cpp_py.client.async_ import LlamaAsyncClient
+from llama_cpp_py.client.sync import LlamaSyncClient
 
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     'LlamaAsyncServer',
     'LlamaReleaseManager',
     'LlamaAsyncClient',
+    'LlamaSyncClient',
 ]
--- llama_cpp_py/client/__init__.py
+++ llama_cpp_py/client/__init__.py
@@ -1,4 +1,4 @@
 from llama_cpp_py.client.async_ import LlamaAsyncClient
 from llama_cpp_py.client.sync import LlamaSyncClient
 
-__all__ = ['LlamaAsyncClient']
+__all__ = ['LlamaAsyncClient', 'LlamaSyncClient']
