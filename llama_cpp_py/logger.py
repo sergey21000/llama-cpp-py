2de6e5715544a5ec6936990d91d8c4e866b230d3
sergey21000 <67040429+sergey21000@users.noreply.github.com>
2026-02-22 09:41:20 +0300

    Update logger.py

--- llama_cpp_py/logger.py
+++ llama_cpp_py/logger.py
@@ -8,16 +8,16 @@ if 0 in logger._core.handlers:
     logger.add(
         sink=sys.stderr,
         level='INFO',
-        filter=lambda record: 'llama_server' in record['extra'],
+        filter=lambda record: record['extra'].get('name') == 'llama_server',
     )
 
-server_logger = logger.bind(llama_server=True)
-debug_logger = logger.bind(llama_debug=True)
+server_logger = logger.bind(name=llama_server)
+debug_logger = logger.bind(name=llama_debug)
 
 log_level = os.getenv('LLAMACPP_LOG_LEVEL')
 if log_level:
     debug_logger.add(
         sink=sys.stderr,
         level=log_level,
-        filter=lambda record: 'llama_debug' in record['extra'],
+        filter=lambda record: record['extra'].get('name') == 'llama_debug',
     )
